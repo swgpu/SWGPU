a9a353d8a37c202ab72c5140b6c2f99865666444
jay19240 <aliyah.raijin@gmail.com>
2023-11-04 12:05:51 +0100

    remove build files

--- node_modules/typedoc-plugin-markdown/dist/options-reader.d.ts
+++
@@ -1,7 +0,0 @@
-import { Options, OptionsReader } from 'typedoc';
-export declare class MarkdownThemeOptionsReader implements OptionsReader {
-    name: string;
-    readonly order = 1000;
-    readonly supportsPackages = false;
-    read(container: Options): void;
-}
--- node_modules/typedoc-plugin-markdown/dist/resources/helpers/comment.d.ts
+++
@@ -1,3 +0,0 @@
-import { MarkdownTheme } from '../../theme';
-export default function (theme: MarkdownTheme): void;
-export declare function readFile(file: string): string;
