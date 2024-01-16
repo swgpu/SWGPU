2cb26557db2716911e590462105787dea9f62057
jay19240 <aliyah.raijin@gmail.com>
2024-01-16 23:33:00 +0100

    add ai 2d fight

--- src/samples/fight/game_screen.js
+++ src/samples/fight/game_screen.js
@@ -230,7 +230,7 @@ async function CREATE_PLAYER2(name) {
     enemyAction: null,
     enemyMinDistance: 90,
     enemyMaxDistance: Infinity,
-    tick: 50,
+    tick: 500,
     then: 0,
     commandName: 'CMD_RUN',
     commandArgs: [],
