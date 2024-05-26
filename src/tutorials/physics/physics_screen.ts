5d529cead60c50a50a56f7c3363a5b3e59b59fab
jay19240 <aliyah.raijin@gmail.com>
2024-05-26 14:42:48 +0200

    improve a little bit physics tutorial

--- src/tutorials/physics/physics_screen.ts
+++ src/tutorials/physics/physics_screen.ts
@@ -1,14 +1,25 @@
 import RAPIER from '@dimforge/rapier3d';
+import { gfx3TextureManager } from '@lib/gfx3/gfx3_texture_manager';
 import { Screen } from '@lib/screen/screen';
+import { Gfx3CameraOrbit } from '@lib/gfx3_camera/gfx3_camera_orbit';
 import { Rapier3D } from '@lib/gfx3_physics/gfx3_physics_rapier';
+import { Gfx3MeshJSM } from '@lib/gfx3_mesh/gfx3_mesh_jsm';
+import { Gfx3Material } from '@lib/gfx3_mesh/gfx3_mesh_material';
 // ---------------------------------------------------------------------------------------
 
+// resource 1: https://github.com/8Observer8/ship-movement-oimophysics-rollup-threejs-js/blob/main/src/index.js
+// resource 2: https://www.youtube.com/watch?v=SWc4tKHVye4
+
 class PhysicsScreen extends Screen {
+  camera: Gfx3CameraOrbit;
   world: RAPIER.World;
   rigidBody: RAPIER.RigidBody;
+  ship: Gfx3MeshJSM;
 
   constructor() {
     super();
+    this.camera = new Gfx3CameraOrbit(0);
+
     let gravity = { x: 0.0, y: -9.81, z: 0.0 };
     this.world = new Rapier3D.World(gravity);
   
@@ -23,22 +34,31 @@ class PhysicsScreen extends Screen {
     // Create a cuboid collider attached to the dynamic rigidBody.
     let colliderDesc = Rapier3D.ColliderDesc.cuboid(0.5, 0.5, 0.5);
     this.world.createCollider(colliderDesc, this.rigidBody);
+
+    this.ship = new Gfx3MeshJSM();
   }
 
   async onEnter() {
+    this.camera.setPosition(0, 0, 10);
+    await this.ship.loadFromFile('./tutorials/physics/ship.jsm');
+    this.ship.setMaterial(new Gfx3Material({
+      texture: await gfx3TextureManager.loadTextureMips('./tutorials/physics/ship.png')
+    }));
   }
 
   update(ts: number) {
+    this.camera.update(ts);
     // Ste the simulation forward.  
     this.world.step();
   
     // Get and print the rigid-body's position.
     let position = this.rigidBody.translation();
+    this.ship.setPosition(position.x, position.y, position.z);
     console.log('Rigid-body position: ', position.x, position.y, position.z);  
   }
 
   draw() {
-    // draw loop.
+    this.ship.draw();
   }
 }
 
