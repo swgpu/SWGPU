1c23c9204afba8b5e80e6db4c842c647c9d2da0b
jay19240 <aliyah.raijin@gmail.com>
2024-07-16 00:58:53 +0200

    little fix

--- src/templates/platformer/platformer_screen.ts
+++ src/templates/platformer/platformer_screen.ts
@@ -20,7 +20,6 @@ import { MovePlatformSystem } from './systems/move_platform';
 import { PlayerControllerSystem } from './systems/player_controller';
 import { PlayerShootSystem } from './systems/player_shoot';
 import { TakeDamageSytem } from './systems/take_damage';
-import { engineManager } from '@lib/engine_manager';
 // ---------------------------------------------------------------------------------------
 
 class PlatformerScreen extends Screen {
