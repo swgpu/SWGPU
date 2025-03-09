b7872fb30d07d4ce7b05785183cfde838b9ff4f5
jay24120 <aliyah.raijin@gmail.com>
2025-03-09 18:46:53 +0100

    update npm repo

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -118,4 +118,5 @@ export type { TextureTarget } from './gfx3_mesh/gfx3_mesh_material';
 export type { Gfx3Viewport, ProjectionMode } from './gfx3/gfx3_view';
 export type { vec1, vec2, vec3, vec4, vec5, vec6, vec_any, mat3, mat4, bounds2, bounds3 } from './core/global';
 export type { Sound } from './sound/sound_manager';
+export type { RenderingMode } from './engine/engine_manager';
 export type { PackItem, PackItemList } from './engine/engine_pack';
